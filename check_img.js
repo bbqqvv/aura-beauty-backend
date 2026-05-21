97a441c377d0513220094cb49e087f1951efa977
BUI QUOC VAN <116750804+bbqqvv@users.noreply.github.com>
2026-05-21 16:39:25 +0700

    chore: remove temporary diagnostics and migration scripts

--- check_img.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Product = require('./model/Products');
-
-mongoose.connect('mongodb://127.0.0.1:27017/shofy').then(async () => {
-  const p = await Product.findOne({ title: /Face Wash/i });
-  console.log('img:', p.img);
-  process.exit(0);
-});
